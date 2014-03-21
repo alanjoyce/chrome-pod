0d47ca3e653b7d6e653985e8239776c5c5e7cabd
Alan Joyce <alan@thisisalan.com>
2014-03-21 13:15:18 -0700

    Adding podcast feed loading.

--- main.js
+++ main.js
@@ -3,8 +3,22 @@ onload = function() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange=function() {
       if(xhr.readyState == 4 && xhr.status == 200) {
+        var xmlDoc = $.parseXML(xhr.responseText);
         console.log(xhr.responseText);
-        $("#feedResults").html(xhr.responseText);
+        
+        $("#feedResultsList").html("");
+        var items = $(xmlDoc).find("item").each(function() {
+          var enclosure = $(this).find("enclosure");
+          $("#feedResultsList").append(
+            "<li>"
+              + $(this).find("title").text() + "<br />"
+              + "<audio controls>"
+              + "<source src='" + enclosure.attr("url")
+              + "' type='" + enclosure.attr("type") + "'>"
+              + "</audio>"
+            + "</li>"
+          );
+        });
       }
     }
     xhr.open("GET", $("#feedURL").val(), true);
