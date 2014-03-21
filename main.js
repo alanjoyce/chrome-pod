899bbefe853f87561b1d3643de877a290715cded
Alan Joyce <alan@thisisalan.com>
2014-03-21 13:27:19 -0700

    Changing a bunch of quotation marks.

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 onload = function() {
-  $("#loadFeed").click(function() {
+  $('#loadFeed').click(function() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange=function() {
       if(xhr.readyState == 4 && xhr.status == 200) {
         var xmlDoc = $.parseXML(xhr.responseText);
         console.log(xhr.responseText);
         
-        $("#feedResultsList").html("");
-        var items = $(xmlDoc).find("item").each(function() {
-          var enclosure = $(this).find("enclosure");
-          $("#feedResultsList").append(
-            "<li>"
-              + $(this).find("title").text() + "<br />"
-              + "<audio controls>"
-              + "<source src='" + enclosure.attr("url")
-              + "' type='" + enclosure.attr("type") + "'>"
-              + "</audio>"
-            + "</li>"
+        $('#feedResultsList').html('');
+        var items = $(xmlDoc).find('item');
+        for(var i = 0; i < items.length && i < 5; i++) {
+          var title = $(items[i]).find('title');
+          var enclosure = $(items[i]).find('enclosure');
+          $('#feedResultsList').append(
+            '<li>'
+              + title.text() + '<br />'
+              + '<audio controls class="podcastPlayer">'
+              + '<source src="' + enclosure.attr('url')
+              + '" type="' + enclosure.attr('type') + '">'
+              + '</audio>'
+            + '</li>'
           );
-        });
+        };
       }
     }
-    xhr.open("GET", $("#feedURL").val(), true);
+    xhr.open('GET', $('#feedURL').val(), true);
     xhr.send();
   });
 }
