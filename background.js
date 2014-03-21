038f1158af8e48e7e799b6cfaa847de0f121dc65
Alan Joyce <alan@thisisalan.com>
2014-03-21 15:59:35 -0700

    Cleanup.

--- background.js
+++ background.js
@@ -3,16 +3,16 @@ chrome.app.runtime.onLaunched.addListener(function() {
   // Center window on screen.
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
-  var width = 250;
+  var width = 350;
   var height = 450;
 
   chrome.app.window.create('index.html', {
-    id: "chromePod",
-    bounds: {
-      width: width,
-      height: height,
-      left: Math.round((screenWidth-width)/2),
-      top: Math.round((screenHeight-height)/2)
-    }
+    id: 'chromePod',
+    width: width,
+    height: height,
+    minWidth: 310,
+    minHeight: 450,
+    left: Math.round((screenWidth-width)/2),
+    top: Math.round((screenHeight-height)/2)
   });
 });
